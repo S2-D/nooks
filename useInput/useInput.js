118e8e49e55dfb5b7fae0c375de380fb26dd9eaa
sodamshin <74418105+S2-D@users.noreply.github.com>
2021-10-24 23:41:59 +0900

    #1.2 useInput part Two

--- useInput/useInput.js
+++ useInput/useInput.js
@@ -1,10 +1,16 @@
-export const useInput = initialValue => {
-    const [value, setValue] = useState(initialValue);
-    const onChange = event => {
-      const {
-        target: { value }
-      } = event;
+export const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = event => {
+    const {
+      target: { value }
+    } = event;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
       setValue(value);
-    };
-    return { value, onChange };
+    }
   };
+  return { value, onChange };
+};
